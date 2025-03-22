4e535457c32907c0ba65b9b6ef200f77c0ebc253
galzmarc <marco.galzignato@gmail.com>
2025-03-22 07:59:58 +0000

    Added Cart class to model cash register

--- models.py
+++ models.py
@@ -27,9 +27,29 @@ class Coffee(Product):
         unit_price = (2 / 3) * self.price if quantity >= 3 else self.price
         return unit_price * quantity
 
+
 # Using a dictionary to store products, mimics database
 products = {
-    "GR1": GreenTea("GR1", "Green Tea", 3.11),
-    "SR1": Strawberries("SR1", "Strawberries", 5.00),
-    "CF1": Coffee("CF1", "Coffee", 11.23),
+    "gr1": GreenTea("GR1", "Green Tea", 3.11),
+    "sr1": Strawberries("SR1", "Strawberries", 5.00),
+    "cf1": Coffee("CF1", "Coffee", 11.23),
 }
+
+
+class Cart:
+    def __init__(self):
+        self.items = {}
+
+    def add(self, product_code, quantity):
+        if product_code in self.items:
+            self.items[product_code] += quantity
+        else:
+            self.items[product_code] = quantity
+    
+    def total(self, products):
+        total_price = 0.0
+        for product_code, quantity in self.items.items():
+            p = products.get(product_code)
+            if p:
+                total_price += p.apply_discount(quantity)
+        return total_price
