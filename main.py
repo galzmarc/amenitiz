ea35a50d7c919e2adc7c6d7c17fa570e986fb1e4
galzmarc <marco.galzignato@gmail.com>
2025-03-22 18:53:16 +0000

    Wrote REST APIs

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-# This will be the backend
+from fastapi import FastAPI
+from models import products, Cart
+
+app = FastAPI()
+cart = Cart()
+
+# API for products
+@app.get("/products/")
+async def get_all():
+    return [
+        {"code": p.product_code, "name": p.name, "price": p.price}
+        for p in products.values()
+    ]
+
+@app.get("/products/{code}")
+async def get_one(code):
+    product = products.get(code)
+    if not product:
+        return {"error": "Product not found"}
+    return {"code": product.product_code, "name": product.name, "price": product.price}
+
+# API for shopping cart
+@app.get("/cart/")
+async def get_cart_total():
+    return {"total": cart.total(products)}
+
+@app.post("/cart/")
+async def add_to_cart(product_code, quantity):
+    if product_code not in products:
+        return {"error": "Product not found"}
+    cart.add(product_code, quantity)
+    return {"message": f"Added {quantity} x {product_code} to cart"}
