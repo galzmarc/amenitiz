895885ecf68e64af0d926b798fc9dbdb2fef9ee0
galzmarc <marco.galzignato@gmail.com>
2025-03-24 13:03:06 +0000

    Refactored the sessions to use FastAPI Dependency

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import Session, SQLModel, create_engine, select
 from .models import Product, Cart, CartItem
 
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 def create_products():
     products = [
@@ -9,7 +12,6 @@ def create_products():
         {"code": "sr1", "name": "Strawberries", "price": 5.00},
         {"code": "cf1", "name": "Coffee", "price": 11.23},
     ]
-
     with Session(engine) as session:
         for prod in products:
             existing_product = session.exec(select(Product).where(Product.code == prod["code"])).first()
@@ -29,64 +31,60 @@ SQLModel.metadata.create_all(engine)
 # Insert products
 create_products()
 
+# ----- API for products -----
 
-# API for products
 @app.get("/products/")
-async def get_all():
-    with Session(engine) as session:
-        statement = select(Product)
-        results = session.exec(statement)
-        return [
-            {"code": res.code, "name": res.name, "price": res.price}
-            for res in results
-        ]
+async def get_all(session: Session = Depends(get_session)):
+    statement = select(Product)
+    results = session.exec(statement)
+    return [
+        {"code": res.code, "name": res.name, "price": res.price}
+        for res in results
+    ]
 
 @app.get("/products/{code}")
-async def get_one(code: str):
-    with Session(engine) as session:
-        statement = select(Product).where(Product.code == code)
-        product = session.exec(statement).first()
-        if not product:
-            return {"error": "Product not found"}
-        return {"code": product.code, "name": product.name, "price": product.price}
-    
+# This endpoint is actually never used, but it might come in handy if we expand the app.
+# Took me 2 secs to write, so there is no point in deleting
+async def get_one(*, session: Session = Depends(get_session), code: str):
+    statement = select(Product).where(Product.code == code)
+    product = session.exec(statement).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return {"code": product.code, "name": product.name, "price": product.price}
 
-# API for shopping cart
+# ----- API for shopping cart -----
+
 @app.get("/cart/")
-async def get_cart_total():
-    with Session(engine) as session:
-        return {"items": cart.items, "total": cart.total(session)}
+async def get_cart_total(session: Session = Depends(get_session)):
+    return {"items": cart.items, "total": cart.total(session)}
     
 @app.post("/cart/")
-async def add_to_cart(item: CartItem):
-    with Session(engine) as session:
-        statement = select(Product).where(Product.code == item.code)
-        product = session.exec(statement).first()
-        if not product:
-            return {"error": "Product not found"}
-        cart.add(item.code, item.quantity)
-        return {"message": f"Added {item.quantity} x {item.code} to cart"}
-    
-@app.delete("/cart/{code}")
-async def delete_from_cart(code: str):
-    with Session(engine) as session:
-        statement = select(Product).where(Product.code == code)
-        product = session.exec(statement).first()
-        if not product:
-            return {"error": "Product not found"}
-        if code not in cart.items:
-            return {"error": "Product not in cart"}
-        cart.remove(code)
-        return {"message": f"Removed {code} from cart"}
+async def add_to_cart(*, session: Session = Depends(get_session), item: CartItem):
+    statement = select(Product).where(Product.code == item.code)
+    product = session.exec(statement).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    cart.add(item.code, item.quantity)
+    return {"message": f"Added {item.quantity} x {item.code} to cart"}
 
 @app.put("/cart/{code}")
-async def update_item(code: str, item: CartItem):
-    with Session(engine) as session:
-        statement = select(Product).where(Product.code == code)
-        product = session.exec(statement).first()
-        if not product:
-            return {"error": "Product not found"}
-        if code not in cart.items:
-            return {"error": "Product not in cart"}
-        cart.update(code, item.quantity)
-        return {"message": f"Updated quantity for {code}: {item.quantity}"}
+async def update_item(*, session: Session = Depends(get_session), code: str, item: CartItem):
+    statement = select(Product).where(Product.code == code)
+    product = session.exec(statement).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    if code not in cart.items:
+        raise HTTPException(status_code=404, detail="Product not in cart")
+    cart.update(code, item.quantity)
+    return {"message": f"Updated quantity for {code}: {item.quantity}"}
+
+@app.delete("/cart/{code}")
+async def delete_from_cart(*, session: Session = Depends(get_session), code: str):
+    statement = select(Product).where(Product.code == code)
+    product = session.exec(statement).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    if code not in cart.items:
+        raise HTTPException(status_code=404, detail="Product not in cart")
+    cart.remove(code)
+    return {"message": f"Removed {code} from cart"}
